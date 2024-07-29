2c5e61ccbf8943756f5d4a9319f7010dcd7baabd
BishalMrzn <112357355+BishalMrzn@users.noreply.github.com>
2024-07-29 22:12:44 +0545

    Add files via upload

---
+++ Hackerman/main.js
@@ -0,0 +1,31 @@
+const color="background:white;color: green;";
+setTimeout(()=>{
+    console.log("%cHacking..",color)
+},1000)
+setTimeout(()=>{
+    console.log("%cTracking all device",color)
+},2000)
+setTimeout(()=>{
+    console.log("%cFound 4 devices",color)
+},3000)
+setTimeout(()=>{
+    console.log("%cFinding Saroj's Facebook account",color)
+},4000)
+setTimeout(()=>{
+    console.log("%cTracking Finished",color)
+},5000)
+setTimeout(()=>{
+    console.log("%cSaroj Bhandari close to you 1 account found",color)
+},6000)
+setTimeout(()=>{
+    console.log("%cFriends true",color)
+},7000)
+setTimeout(()=>{
+    console.log("%cHacking FInished",color)
+},8000)
+setTimeout(()=>{
+    console.log("%cCracking Password",color)
+},9000)
+setTimeout(()=>{
+    console.log("%cCracking finished",color)
+},10000);
